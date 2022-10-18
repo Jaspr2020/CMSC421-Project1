a11ae4c4bfd2b116cf3602e77e2aeacfe7e67a59
Jaspr2020 <jsr5577@gmail.com>
2022-10-17 22:00:20 -0400

    Created ParentProcess and LikeServers
    ParentProcess creates 10 LikeServers. Like servers write to a log file and lives for whatever amount of time is set. LikeServers write to log file on creation and exit.

--- likeServer.c
+++ likeServer.c
@@ -1,16 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
 
-int test();
+void CreateLog();
+void WriteLog(char *);
+void ParentProcess();
+int LikeServer(int);
 
 int main() {
-    test();
+    ParentProcess();
     return 0;
 }
 
-int test() {
-    fork();
-    printf("Hello World!");
-    return 0;
+void ParentProcess() {
+    CreateLog();
+
+    for (int ii = 0; ii < 10; ii++) {
+        LikeServer(ii);
+        sleep(1);
+    }
+
+    // printf("Hello from Parent!\n");
+}
+
+void CreateLog() {
+    // Create or clear log file
+    FILE *fp;
+    fp = fopen("./tmp/ParentProcessStatus.txt","w");
+    fclose(fp);
+}
+
+void WriteLog(char *myString) {
+    // Open log file and write string
+    FILE *fp;
+    fp = fopen("./tmp/ParentProcessStatus.txt","a");
+    fprintf(fp, "%s", myString);
+    fclose(fp);
+}
+
+int LikeServer(int serverNumber) {
+    // Create child process
+    int id = fork();
+    
+    // Child process
+    if (id == 0) {
+        // Seconds process lasts
+        int lifetime = 5;
+        clock_t start = clock();
+
+        char buffer[30];
+        sprintf(buffer, "Child %d started\n", serverNumber);
+        WriteLog(buffer);
+
+        while ((clock() - start) / CLOCKS_PER_SEC < lifetime) {
+
+        }
+        // sleep(5);
+
+        sprintf(buffer, "Child %d ended\n", serverNumber);
+        WriteLog(buffer);
+        exit(0);
+    } 
+    return id;
 }
--- makefile
+++ makefile
@@ -2,4 +2,4 @@ all:
 	gcc -Wall -o likeServer likeServer.c
 
 run:
-	likeServer
+	./likeServer
